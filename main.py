fc2fe6b71e5bad918b0aac160714474d67610542
Mason <lixq@weilaicheng.com>
2020-09-20 10:38:35 +0800

    init project

---
+++ main.py
@@ -0,0 +1,4 @@
+from testcases import testcase01
+
+if __name__ == '__main__':
+    testcase01.test1()
---
+++ testcases/testcase01.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+
+def test1():
+    print('first test case run')
